385779cafb5e1d1d31ce224699b902d16d7edd5b
Oleksii_Lukash <Oleksii_Lukash@epam.com>
2016-03-22 03:22:52 -0400

    test method add

--- src/main/java/org/springframework/samples/petclinic/util/EntityUtils.java
+++ src/main/java/org/springframework/samples/petclinic/util/EntityUtils.java
@@ -51,6 +51,6 @@ public abstract class EntityUtils {
         throw new ObjectRetrievalFailureException(entityClass, entityId);
     }
 
-    public void testMethodw2s2f(){}
+    public void testMethodw(){}
 
 }
